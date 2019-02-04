01e017d46b9486710be45dd4e05b743a965eb33c
markonline37 <markonline37@gmail.com>
2019-02-04 20:09:55 +0000

    Started framework
    Initial commit of header and footer files, need to update them further.

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo($title); ?></title>
+	<meta name="description" content="<?php echo($description); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" href="images/favicon.ico"/>
+
+	//placeholder
+	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+	<script type=\"text/javascript\" src=\"js/universities.js\"></script>
+	<script type=\"text/javascript\" src=\"js/job_explorer.js\"></script>
+	<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
+	<script type=\"text/javascript\" src=\"js/course_statistics.js\"></script>
+</head>
+<body id="body">
+<a id="top_anchor"></a>
+<div id="centre_website">
+<div id="content">
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$title = 'Homepage';
+$description = 'Website Homepage';
+$currentPage = 'homepage';
+include('inc/header.php');
+?>
+
+//shit goes here
+
+<?php
+include('inc/footer.php'); 
+?>
