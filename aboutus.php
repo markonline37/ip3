d0f2e4e0d7b9a222ffd1ce08c06b4ec3d8e8c9ae
Mark <markonline37@gmail.com>
2019-03-10 17:49:05 +0000

    framework & index push
    updated framework, nav, js, index etc.

---
+++ aboutus.php
@@ -0,0 +1,12 @@
+<?php 
+$title = 'About Us Page';
+$description = 'About Us';
+$currentPage = 'aboutus';
+include('inc/header.php');
+?>
+
+Hello World!
+
+<?php
+include('inc/footer.php'); 
+?>
--- inc/header.php
+++ inc/header.php
@@ -2,17 +2,18 @@
 <html>
 <head>
 	<title><?php echo($title); ?></title>
-	<meta name="description" content="<?php echo($description); ?>">
+	<?php echo"<meta name=\"description\" content=\"".$description."\""?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
 	<link href="css/style.css" rel="stylesheet" type="text/css">
 	<link rel="shortcut icon" href="images/favicon.ico"/>
 
-	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
-	<script type=\"text/javascript\" src=\"js/nav.js\"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/nav.js"></script>
 </head>
 <body>
 <a id="top_anchor"></a>
+<button onclick="topFunction()" id="bottom_button" class="top_button" title="Go to top">Top</button>
 <div id="centre_website">
 <div id="content">
 	<div id="navigation" class="mainNavigation">
@@ -20,10 +21,28 @@
 		<ul class="navigation">
 			<li class="navigation"><a class="navigation" href="index.php">Home</a><?php if($currentPage === 'homepage') echo "<div id=\"active_rectangle\"></div>" ?></li>
 			<li class="navigation">|</li>
+
+			<li class="navigation"><a class="navigation" href="aboutus.php">About Us</a><?php if($currentPage === 'aboutus') echo "<div id=\"active_rectangle\"></div>" ?></li>
+			<li class="navigation">|</li>
+
 			<li class="navigation"><a class="navigation" href="earthquake.php">Earthquake</a><?php if($currentPage === 'earthquake') echo "<div id=\"active_rectangle\"></div>" ?></li>
 			<li class="navigation">|</li>
+
 			<li class="navigation"><a class="navigation" href="weather.php">Weather</a><?php if($currentPage === 'weather') echo "<div id=\"active_rectangle\"></div>" ?></li>
 			<li class="navigation">|</li>
+
+			<li class="navigation"><a class="navigation" href="videogames.php">Video Games</a><?php if($currentPage === 'videogames') echo "<div id=\"active_rectangle\"></div>" ?></li>
+			<li class="navigation">|</li>
+
+			<li class="navigation"><a class="navigation" href="pokemon.php">Pokémon</a><?php if($currentPage === 'pokemon') echo "<div id=\"active_rectangle\"></div>" ?></li>
+			<li class="navigation">|</li>
+
+			<li class="navigation"><a class="navigation" href=".......">.......</a><?php if($currentPage === '') echo "<div id=\"active_rectangle\"></div>" ?></li>
+			<li class="navigation">|</li>
+
+			<li class="navigation"><a class="navigation" href=".......">.......</a><?php if($currentPage === '') echo "<div id=\"active_rectangle\"></div>" ?></li>
+			<li class="navigation">|</li>
+			
 			<li class="navigation"><a class="navigation" href="tutorial.php">Tutorials</a><?php if($currentPage === 'tutorial') echo "<div id=\"active_rectangle\"></div>" ?></li>
 		</ul>
 	</div>
