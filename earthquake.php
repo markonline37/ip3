c56a202bff092dd1def12e785a9f6f9756f851ee
Merin Haslacher <mhasla200@caledonian.ac.uk>
2019-03-18 00:50:03 +0000

    Dynamically Created Buttons

--- earthquake.php
+++ earthquake.php
@@ -12,16 +12,10 @@ include('inc/header.php');
 
 
 <h1>Earthquake Mapping</h1>
-Select time then magnitude<br>
-<button type="button" id="hour">Past hour</button>
-<button type="button" id="day">Past Day</button>
-<button type="button" id="7days">Past 7 Days</button>
-<button type="button" id="30days">Past 30 days</button>
-<br>
-<button type="button" id="sig">Significant</button>
-<button type="button" id="m45">M4.5+</button>
-<button type="button" id="m25">M2.5+</button>
-<button type="button" id="m10">M1.0+</button>
+<div id="feedColumn">
+    <div id="feedSelector"></div>
+</div>
+
 </div>
 <div id="map">
     
--- js/earthquake.js
+++ js/earthquake.js
@@ -10,126 +10,72 @@ function initMap() {
 
 var thelocation;
 var titleName;
-var url;
-var time;
+
+var quakeFeeds = {
+    "past hour": {
+        "all earthquakes": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
+        "all 1.0+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson",
+        "all 2.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson",
+        "all 4.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson",
+        "all significant": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson"
+    },
+    "past day": {
+        "all earthquakes": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
+        "all 1.0+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson",
+        "all 2.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson",
+        "all 4.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson",
+        "all significant": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson"
+    },
+    "past week": {
+        "all earthquakes": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
+        "all 1.0+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson",
+        "all 2.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson",
+        "all 4.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+        "all significant": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+    },
+    "past month": {
+        "all earthquakes": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson",
+        "all 1.0+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson",
+        "all 2.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson",
+        "all 4.5+": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson",
+        "all significant": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+
+    }
+}
+
+function makeChildProps(obj, currentProp) {
+    var childProps = '';
+
+    for (var prop in obj[currentProp]) {
+        var el = "<div class='child-prop'><button class='feed-name' data-feedurl='" + obj[currentProp][prop] + "'>" + prop + "</button></div>";
+        childProps += el;
+    }
+
+    return childProps;
+}
 
 $(document).ready(function () {
 
-    $('#hour').click(function () {
-        url= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
-        time= url;
-        update();
-    });
-    $('#day').click(function () {
-        url= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-        time= url;
-        update();
-    });
-    $('#7days').click(function () {
-        url= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-        time= url;
-        update();
-    });
-    $('#30days').click(function () {
-        url= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
-        time= url;
-        update();
-    });
-    $('#sig').click(function () {
-        if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-        }
-        else{
-            throw "Please select a time range";
-            return;
-        }
-        update();
-    });
-    $('#m45').click(function () {
-        if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-        }
-        else{
-            throw "Please select a time range";
-            return;
-        }
-        update();
-    });
-    $('#m25').click(function () {
-        if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
-        }
-        else{
-            throw "Please select a time range";
-            return;
-        }
-        update();
-    });
-    $('#m10').click(function () {
-        if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
-        }
-        else if(time == "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"){
-            url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
-        }
-        else{
-            throw "Please select a time range";
-            return;
-        }
-        update();
-    });
+    for (var prop in quakeFeeds) {
 
-});
+        if (!quakeFeeds.hasOwnProperty(prop)) {
+            continue;
+        }
 
+        $('#feedSelector').append("<div class='feed-date'>" + prop + "</div>" + makeChildProps(quakeFeeds, prop));
+        console.log(makeChildProps(quakeFeeds, prop));
+    }
 
+    $('.feed-name').click(function (e) {
 
-function update(){  
-        
-        // Set Google map  to its start state
         map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 2,
-            center: new google.maps.LatLng(2.8, -187.3), // Center Map. Set this to any location that you like
-            mapTypeId: 'terrain' // can be any valid type
-        });  
+        zoom: 2,
+        center: new google.maps.LatLng(2.8, -187.3), // Center Map. Set this to any location that you like
+        mapTypeId: 'terrain' // can be any valid type
+        }); 
 
-        // The following uses JQuery library
         $.ajax({
-            // The URL of the specific data required
-            url: url,
-
+            url: $(e.target).data('feedurl'), // The GeoJSON URL associated with a specific button was stored in the button's properties when the button was created
             // Called if there is a problem loading the data
             error: function () {
                 $('#info').html('<p>An error has occurred</p>');
@@ -152,5 +98,7 @@ function update(){
                 var markerCluster = new MarkerClusterer(map, markers,
                     { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
             }
+
         });
-    }
+    });
+});
