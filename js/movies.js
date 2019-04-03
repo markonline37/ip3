a335f85ed096f9b85f34b606886a212e4cea6d13
Merin Haslacher <mhasla200@caledonian.ac.uk>
2019-04-03 20:50:54 +0100

    Update movies.js

--- js/movies.js
+++ js/movies.js
@@ -4,102 +4,124 @@ $(document).ready(function (){
 	load();
 });
 
+var movieLinks = [];
+var myPieChart;
+
+function loadChart(){
+	var labels = ["Action","Adventure","Animation","Comedy","Crime","Documentary","Drama","Fantasy","History","Horror","Mystery","Romance","ScienceFiction","Thriller","Western"];
+	
+	var ctx = document.getElementById('chart');
+		myPieChart = new Chart(ctx, {
+	    type: 'pie',
+	    data: {
+	    	labels: labels,
+	        datasets: [{
+		        label: "Movies with Genre",
+		        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", "#223344", "#ffa500", "#bebebe" , "#f1c2c2", "#fa7268", "#85a8c2", "#91a490", "#77c8c0", "#cdb0b0", "#7b9e83"],
+	        }]	
+	    },
+	    options: {
+	      	title: {
+		        display: true,
+		        text: 'Genres of top 100 grossing movies',
+		        fontSize: 40
+	    	},
+	    	legend: {
+	    		display: false
+	    	},
+	    	tooltips: {
+	    		titleFontSize: 20,
+	    		bodyFontSize: 20
+	    	}
+	    	
+	    }
+
+	});
+}
+
+function generateLinks(){
+
+	for(i = 1; i <= 5; i++){
+		movieLinks.push("https://api.themoviedb.org/3/discover/movie?api_key=fd710eb4aa46d9d9a6a5c6d5192149c4&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page="+i);
+	}
+
+}
+
+var amount = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 function load() {
 
-	$.ajax({
-		url: "https://api.themoviedb.org/3/discover/movie?api_key=fd710eb4aa46d9d9a6a5c6d5192149c4&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=1",
-		dataType: 'json',    
-        success: function(data){
-        	var genres = {Action:0,Adventure:0,Animation:0,Comedy:0,Crime:0,Documentary:0,Drama:0,Fantasy:0,History:0,Horror:0,Mystery:0,Romance:0,ScienceFiction:0,Thriller:0,Western:0};
-        	$.each(data.results, function(i){
-				var genre_ids = data.results[i].genre_ids;
-				for (j in genre_ids){
-					switch(genre_ids[j]){
-						case 28:
-							genres.Action += 1;
-							break;
-						case 12:
-							genres.Adventure += 1;
-							break;
-						case 16:
-							genres.Animation += 1;
-							break;
-						case 35:
-							genres.Comedy += 1;
-							break;
-						case 80:
-							genres.Crime += 1;
-							break;
-						case 99:
-							genres.Documentary += 1;
-							break;
-						case 18:
-							genres.Drama += 1;
-							break;
-						case 14:
-							genres.Fantasy += 1;
-							break;
-						case 36:
-							genres.History += 1;
-							break;
-						case 27:
-							genres.Horror += 1;
-							break;
-						case 9648:
-							genres.Mystery += 1;
-							break;
-						case 10749:
-							genres.Romance += 1;
-							break;
-						case 878:
-							genres.ScienceFiction += 1;
-							break;
-						case 53:
-							genres.Thriller += 1;
-							break;
-						case 37:
-							genres.Western += 1;
-							break;
+	loadChart();
+	generateLinks();
+
+	$.each(movieLinks, function(i,item){
+	    $.ajax({
+	    	url: item,
+			dataType: 'json',    
+	        success: function(data){
+	        	$.each(data.results, function(i){
+					var genre_ids = data.results[i].genre_ids;
+					for (j in genre_ids){
+						switch(genre_ids[j]){
+							case 28://Action
+								amount[0] += 1;
+								break;
+							case 12://Adventure
+								amount[1] += 1;
+								break;
+							case 16://Animation
+								amount[2] += 1;
+								break;
+							case 35://Comedy
+								amount[3] += 1;
+								break;
+							case 80://Crime
+								amount[4] += 1;
+								break;
+							case 99://Documentary
+								amount[5] += 1;
+								break;
+							case 18://Drama
+								amount[6] += 1;
+								break;
+							case 14://Fantasy
+								amount[7] += 1;
+								break;
+							case 36://History
+								amount[8] += 1;
+								break;
+							case 27://Horror
+								amount[9] += 1;
+								break;
+							case 9648://Mystery
+								amount[10] += 1;
+								break;
+							case 10749://Romance
+								amount[11] += 1;
+								break;
+							case 878://Science Fiction
+								amount[12] += 1;
+								break;
+							case 53://Thriller
+								amount[13] += 1;
+								break;
+							case 37://Western
+								amount[14] += 1;
+								break;
+						}
 					}
-				}
-			});
-			//document.getElementById("data").innerHTML = JSON.stringify(genres);
-			var values = [genres.Action,genres.Adventure,genres.Animation,genres.Comedy,genres.Crime,genres.Documentary,genres.Drama,genres.Fantasy,genres.History,genres.Horror,genres.Mystery,genres.Romance,genres.ScienceFiction,genres.Thriller,genres.Western];
-			var labels = ["Action","Adventure","Animation","Comedy","Crime","Documentary","Drama","Fantasy","History","Horror","Mystery","Romance","ScienceFiction","Thriller","Western"];
-        	
-        	var ctx = document.getElementById('chart');
-        	var myPieChart = new Chart(ctx, {
-			    type: 'pie',
-			    data: {
-			    	labels: labels,
-			        datasets: [{
-				        label: "Games with Genre",
-				        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", "#223344", "#ffa500", "#bebebe" , "#f1c2c2", "#fa7268", "#85a8c2", "#91a490", "#77c8c0", "#cdb0b0", "#7b9e83"],
-				        data: values
-			        }]	
-			    },
-			    options: {
-			      	title: {
-				        display: true,
-				        text: 'Genres of 20 top grossing movies',
-				        fontSize: 40
-			    	},
-			    	legend: {
-			    		display: false
-			    	},
-			    	tooltips: {
-			    		titleFontSize: 20,
-			    		bodyFontSize: 20
-			    	}
-			    	
-			    }
-    
-			});
-        },
-        error: function() {
+					
+				});
+	        	myPieChart.data.datasets[0].data = amount;
+    			myPieChart.update();
+	    	},
+	    	error: function() {
             var errorText = "Error occured, please reload page or contact systems administrator";
 	        document.getElementById("info").innerHTML = errorText;
-        }
-	});
+        	}
+        });
+    });
+
 }
+
+
