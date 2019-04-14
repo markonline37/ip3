02f146ad3b2f6585decc3cc9c8d44f6e9060d5c7
Mark <markonline37@gmail.com>
2019-04-14 01:41:35 +0100

    tutorial push

--- shakespearetutorial.php
+++ shakespearetutorial.php
@@ -4,7 +4,45 @@ $description = 'Shakespeare Tutorial';
 include('inc/header.php');
 ?>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="css/tutorial.css" rel="stylesheet" type="text/css">
+
 <h1>Shakespeare Tutorial</h1>
+<p>
+	This is the tutorial written for the <a href="shakespeare.php">Shakespeare visualisation</a><br><br>
+
+	The Shakespeare visualisation works by creating a frequency map of each individual word and counting each occurance. These occurances are then plotted into a chart and their font size and font weight are evaluated verses the maximum occurance.<br><br>
+
+	This tutorial expects a basic understanding of JavaScript, if you need to learn the basics or a refresher; <a href="https://www.w3schools.com/js/js_intro.asp">w3schools - JavaScript Introduction</a> is an excellent choice.
+</p>
+<br>
+<h5>Menu</h5>
+<p>
+	<ul>
+		<li><a href="#section1_anchor">Gathering The Data</a></li>
+		<li><a href="#section2_anchor">Displaying The Data</a></li>
+		<li><a href="#section3_anchor">References</a></li>
+	</ul>
+</p>
+
+<a id="section1_anchor"></a>
+<h5>Gathering The Data</h5>
+<p>
+	
+</p>
+
+<a id="section2_anchor"></a>
+<h5>Displaying The Data</h5>
+<p>
+	
+</p>
+
+
+<a id="section3_anchor"></a>
+<h5>References</h5>
+<p>
+	
+</p>
 
 <?php
 include('inc/footer.php'); 
